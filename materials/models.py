da4eaaff13450e8766bd3586917bab9f7a393853
VladimirKovaliev <kovaliev.v@list.ru>
2024-01-23 19:41:32 +0300

    add model materials

---
+++ materials/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+from users.models import NULLABLE
+
+
+class Course(models.Model):
+    title = models.CharField(max_length=100, verbose_name='навзание')
+    preview = models.ImageField(upload_to='course/', verbose_name='превью', **NULLABLE)
+    description = models.TextField(verbose_name='описание', **NULLABLE)
+
+    def __str__(self):
+        return f'{self.title}: {self.description}'
+
+    class Meta:
+        verbose_name = 'курс'
+        verbose_name_plural = 'курсы'
+
+
+class Lesson(models.Model):
+    title = models.CharField(max_length=100, verbose_name='навзание')
+    description = models.TextField(verbose_name='описание', **NULLABLE)
+    preview = models.ImageField(upload_to='lesson/', verbose_name='превью', **NULLABLE)
+    video_link = models.CharField(max_length=200, verbose_name='ссылка на видео', **NULLABLE)
+
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name='курс')
+
+    def __str__(self):
+        return f'{self.title}: {self.description}'
+
+    class Meta:
+        verbose_name = 'урок'
+        verbose_name_plural = 'уроки'
