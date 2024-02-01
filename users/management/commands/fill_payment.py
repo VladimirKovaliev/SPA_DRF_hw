ab207159a1da5d6a437ac8778c5c048dcc6eca2e
VladimirKovaliev <kovaliev.v@list.ru>
2024-02-01 16:16:23 +0300

    migrations, new serializers

---
+++ users/management/commands/fill_payment.py
@@ -0,0 +1,23 @@
+from django.core.management import BaseCommand
+
+from materials.models import Course, Lesson
+from users.models import Payment, User
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        payment_for_create = []
+
+        for user in User.objects.all():
+            payment_for_create.append(Payment(user=user,
+                                              lesson=Lesson.objects.get(pk=2),
+                                              payment_amount=30000,
+                                              payment_method='cash'))
+        for user in User.objects.all():
+            payment_for_create.append(Payment(user=user,
+                                              course=Course.objects.get(pk=2),
+                                              payment_amount=40000,
+                                              payment_method='transfer'))
+
+        Payment.objects.bulk_create(payment_for_create)
