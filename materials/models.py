bc99fed81706f6d13e12a3f9c5af2b5759979342
VladimirKovaliev <kovaliev.v@list.ru>
2024-02-08 19:39:17 +0300

    add owner

--- materials/models.py
+++ materials/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from django.conf import settings
+
 NULLABLE = {'null': True, 'blank': True}
 
 
@@ -7,6 +9,8 @@ class Course(models.Model):
     title = models.CharField(max_length=100, verbose_name='навзание')
     preview = models.ImageField(upload_to='course/', verbose_name='превью', **NULLABLE)
     description = models.TextField(verbose_name='описание', **NULLABLE)
+
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, **NULLABLE)
 
     def __str__(self):
         return f'{self.title}: {self.description}'
@@ -21,6 +25,8 @@ class Lesson(models.Model):
     description = models.TextField(verbose_name='описание', **NULLABLE)
     preview = models.ImageField(upload_to='lesson/', verbose_name='превью', **NULLABLE)
     video_link = models.CharField(max_length=200, verbose_name='ссылка на видео', **NULLABLE)
+
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, **NULLABLE)
 
     course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name='курс')
 
