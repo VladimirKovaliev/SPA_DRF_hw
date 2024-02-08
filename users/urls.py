f140a5e19a071fbe2555c7a45fc956daeb688eb9
VladimirKovaliev <kovaliev.v@list.ru>
2024-02-08 19:41:44 +0300

    add app_name

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,10 @@
-from django.urls import path, include
+from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
+from users.apps import UsersConfig
 from users.views import PaymentListAPIView
+
+app_name = UsersConfig.name
 
 urlpatterns = [
     path('payment/', PaymentListAPIView.as_view(), name='payment_list'),
