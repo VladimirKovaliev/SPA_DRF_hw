679dd85f8ed2224919acbdc2fa4bfe2b17e7aadb
VladimirKovaliev <kovaliev.v@list.ru>
2024-02-08 16:37:34 +0300

    hw 25.1

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from users.views import PaymentListAPIView
 
 urlpatterns = [
     path('payment/', PaymentListAPIView.as_view(), name='payment_list'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
