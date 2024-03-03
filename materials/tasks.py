68b51fc0fd41ae9810b5113bd3a878e558d477bf
VladimirKovaliev <kovaliev.v@list.ru>
2024-03-03 17:48:55 +0300

    фоновая задача для блокировки неактивных пользователей

--- materials/tasks.py
+++ materials/tasks.py
@@ -1,7 +1,10 @@
+from datetime import timezone
+
 from celery import shared_task
 from django.core.mail import send_mail
 
 from config import settings
+from users.models import User
 
 
 @shared_task
@@ -12,3 +15,15 @@ def update_alert(email, course):
         from_email=settings.EMAIL_HOST_USER,
         recipient_list=[email]
     )
+
+
+@shared_task
+def user_deactivate():
+    now = timezone.now
+    users_list = User.objects.all()
+
+    for user in users_list:
+        day_diff = (now - user.last_login).days
+        if day_diff > 30:
+            user.is_active = False
+            user.save()
